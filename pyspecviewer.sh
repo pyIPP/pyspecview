e8aceb981e39c6e8741c5279188721d39217e257
Tomas Odstrcil <odstrcilt@fusion.gat.com>
2023-06-02 14:41:23 -0700

    fix rotation tomography

---
+++ pyspecviewer.sh
@@ -0,0 +1,25 @@
+ 
+ 
+
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/fusion/projects/codes/pyspecview/FFTW3/lib/
+##export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/fusion/projects/codes/pyspecview/FFTW3/include/
+#export C_INCLUDE_PATH=$C_INCLUDE_PATH:/fusion/projects/codes/pyspecview/FFTW3/lib/
+#export C_INCLUDE_PATH=$C_INCLUDE_PATH:/fusion/projects/codes/pyspecview/FFTW3/include/
+#export LIBRARY_PATH=$LIBRARY_PATH:/fusion/projects/codes/pyspecview/FFTW3/lib/
+#export LIBRARY_PATH=$LIBRARY_PATH:/fusion/projects/codes/pyspecview/FFTW3/include/
+
+ 
+#export PYTHON=/fusion/usc/opt/python/2.7.11/bin/python2.7 
+#export PYTHONPATH="${PYTHONPATH}:/fusion/usc/lib"
+#export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/fusion/usc/opt/python/2.7.11/lib"
+export PATH="${PATH}:/fusion/projects/codes/pyspecview/"
+
+module purge  
+#module load mdsplus 
+#module load fftw
+module load omfit
+
+
+echo ' If you find any bugs, please sent email to odstrcilt@fusion.gat.com'
+
+python  /fusion/projects/codes/pyspecview/pyspecview.py  $@
